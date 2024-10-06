09d18efa10438ba1345f1baaa0f35f77220c509d
callmedukey <iamdevduke@gmail.com>
2024-10-06 15:31:50 +0900

    tidi

--- app/[locale]/(admin)/layout.tsx
+++ app/[locale]/(admin)/layout.tsx
@@ -3,7 +3,7 @@ import { redirect } from "@/i18n/routing";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await verifySession();
-  if (session?.accountType !== "Admin") {
+  if (!session || session?.accountType !== "Admin") {
     redirect("/");
   }
   return <main className="px-4 py-16">{children}</main>;
