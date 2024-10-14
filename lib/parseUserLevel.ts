c616b9e2e4b761d8bbff83616d9a7dc7f46c8005
Joao Santiago <jmmf.santiago94@gmail.com>
2024-10-14 13:48:32 -0300

    push

---
+++ lib/parseUserLevel.ts
@@ -0,0 +1,8 @@
+import type { UserLevel } from "@prisma/client"
+
+export const userLevelObject = {
+    "tip_white":'TIP White',
+    "tip_blue": "TIP Blue",
+    "tip_black": "TIP Black",
+    "tip_gold" : "TIP Gold" }
+
