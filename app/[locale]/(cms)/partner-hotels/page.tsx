79c064089571bf16aae63339670d13090c3e60b5
callmedukey <87065409+callmedukey@users.noreply.github.com>
2024-11-17 11:38:03 +0900

    Test

--- app/[locale]/(cms)/partner-hotels/page.tsx
+++ app/[locale]/(cms)/partner-hotels/page.tsx
@@ -401,7 +401,7 @@ const page = async () => {
         </section>
       </article>
       {uniqueCountries
-        .sort((a, b) => a.localeCompare(b))
+        .sort((a, b) => a.localeCompare(b, locale === "ko" ? "ko" : "en"))
         .map((country) => (
           <article key={country} className="mt-32 max-w-screen-8xl mx-auto">
             <h3 className="text-[2rem] font-semibold leading-normal text-left font-inter mb-16">
