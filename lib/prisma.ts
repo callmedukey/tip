cdb7fb44d87e2b09b9b991510a403fe057427003
callmedukey <87065409+callmedukey@users.noreply.github.com>
2024-12-04 23:40:58 +0900

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,22 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
+const prisma =
+  global.prisma ||
   new PrismaClient({
-    log: ['query', 'error', 'warn'],
+    log: ["query", "info", "warn", "error"],
     datasources: {
       db: {
-        url: process.env.DATABASE_URL
+        url: process.env.DATABASE_URL,
       },
     },
-    connectionTimeout: 20000,
-  })
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV === "development") {
+  global.prisma = prisma;
+}
+
+export default prisma;
